05a8e35502397278ec6dbe777dc8510208e7ee9c
Rodrigo Quiroz Castro <rodrigoquirozcastro@MacBook-Air-de-Rodrigo.local>
2024-12-14 17:02:55 -0300

    JS functions challenge with diverse kind of functions in action. There are exercises whit functions declarations, function expressions and arrow functions

---
+++ ejercicio_1/1_functions.js
@@ -0,0 +1,11 @@
+/* //Transforma esta declaración de función a una función de expresión
+function example(a, b, c) {
+  return a + b + c;
+} */
+
+//solucion
+const example = function (a, b, c) {
+  return a + b + c;
+};
+
+console.log(example(1, 33, 5));
---
+++ ejercicio_2/2_functions.js
@@ -0,0 +1,11 @@
+//Transforma las siguiente función a una arrow function de una línea.
+
+/* suma = function (a, b) {
+  return a + b;
+}; */
+
+//solucion
+
+suma = (a, b) => a + b;
+
+console.log(suma(4, 67));
